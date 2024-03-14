6368f9f3d3ec8393950b724d336697e0088b6a1b
jongithub59 <jdelrealj01@gmail.com>
2024-03-14 15:10:53 -0600

    added styling and added weather icons for forecast

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const search = document.querySelector('input')
-const weather = document.querySelector('.search')
+const weather = document.querySelector('.forecast-results')
 const errorMessage = document.querySelector('.error');
+const icon = document.querySelector('.weather-icon')
 
 search.addEventListener("keypress", (e) => {
   if (e.key === "Enter") getWeather(search, displayWeather);
@@ -18,11 +19,16 @@ search.addEventListener("keypress", (e) => {
      displayWeather(forecast)
    } catch (error) {
      console.log(error)
+     if (search.value === '') return
      errorMessage.textContent = "Could not get weather data";
+     weather.textContent = ''
+     icon.style.display = 'none'
    }
  }
 
 function displayWeather(forecast) {
      errorMessage.textContent = "";
-     weather.textContent = `Forecast for ${forecast.location.name}, ${forecast.location.country}: Temperature: ${forecast.current.temp_f}°F/${forecast.current.temp_c}°C, ${forecast.current.condition.text}`;
-}
+     weather.textContent = `Forecast for ${forecast.location.name}, ${forecast.location.country}: \n Temperature: ${forecast.current.temp_f}°F/${forecast.current.temp_c}°C, ${forecast.current.condition.text}`;
+     icon.src = forecast.current.condition.icon
+     icon.style.display = 'initial'
+    }
